008748a100027eebbcf7b3f645fcf23f1a64db8f
Nikandros <m.nik1997@hotmail.com>
2020-10-10 01:00:14 +0100

    started using pipenv instead of virtualenv

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='Session 2.0',
-    py_modules=['session'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
+    name="Session 2.0",
+    py_modules=["session"],
+    install_requires=["click==7.1.2"],
+    entry_points="""
         [console_scripts]
         session=session:cli
-    '''
+    """,
 )
