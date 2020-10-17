c2a0bb3aa47966e16aa54f865460233cefa6f220
Nikandros <m.nik1997@hotmail.com>
2020-10-17 19:23:22 +0100

    file handling for when files do not exist

--- App.py
+++ App.py
@@ -2,10 +2,12 @@ import sys, appdirs, os
 from appdirs import AppDirs
 
 class HelperFunctions():
+    # create empty file
     def touch(self, path):
         with open(path, 'a'):
             os.utime(path, None)
 
+    # checks if session name exists, terminate program otherwise
     def check_for_session_name(self, session_name):
         if not session_name:
             print('Session name not provided. You cannot use this method without passing a session name in the constructor')
@@ -14,11 +16,26 @@ class HelperFunctions():
 Helpers = HelperFunctions()
 
 class App():
-
     def __init__(self, session_name=None):
         self.session_name = session_name
         self.file_storage = AppDirs('Sessions').user_data_dir
 
+        # create session directory
+        if not os.path.exists(self.file_storage):
+            os.mkdir(self.file_storage)
+        
+        if self.session_name:
+            self.create_file('sessions.ignore')
+            self.create_file(f'{self.session_name}-browser.ses')
+            self.create_file(f'{self.session_name}-software.ses')
+
+    def create_file(self, file_name):
+        delimeter = '/' if '/' in self.file_storage else '\\'
+
+        file = delimeter.join([self.file_storage, file_name])
+
+        if not os.path.exists(file):
+            Helpers.touch(file)
 
 
     def ignore(self, ignored_app):
