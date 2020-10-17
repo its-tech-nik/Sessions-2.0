1e272734aaa46e36432254bf9463606d71972ab5
Nikandros <m.nik1997@hotmail.com>
2020-10-17 17:02:39 +0100

    variable names changed for readability reasons

--- session.py
+++ session.py
@@ -14,35 +14,35 @@ class Config(object):
 pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.command()
-@click.option('-s', default=None, type=click.STRING, help='Store a session')
-@click.option('-r', default=None, type=click.STRING, help='Restore a session')
-@click.option('-i', default=None, type=click.STRING, help='Ignores from storing an app in a session')
-@click.option('-n', default=None, type=click.STRING, help='Used only with -i to specify a session name')
-@click.option('-la', default=None, type=click.STRING, help='List all active sessions')
-@click.option('-a', default=None, type=click.STRING, help='Display all running apps')
+@click.option('-s', 'store', default=None, type=click.STRING, help='Store a session')
+@click.option('-r', 'restore', default=None, type=click.STRING, help='Restore a session')
+@click.option('-i', 'ignore', default=None, type=click.STRING, help='Ignores from storing an app in a session')
+@click.option('-n', 'name', default=None, type=click.STRING, help='Used only with -i to specify a session name')
+@click.option('-ls', 'list_sessions', default=None, type=click.STRING, help='List all active sessions')
+@click.option('-a', 'list_all_apps', default=None, type=click.STRING, help='Display all running apps')
 # @click.option('-d', default=None, type=click.STRING, help='Decouples storage of apps from browser tabs')
-def cli(s, r, i, n, la, a):
+def cli(store, restore, ignore, name, list_sessions, list_all_apps):
     f_params = [v for v in locals().keys() if not v == 'config']
     params = []
     for v in f_params:
         if not locals()[v] is None:
             params.append(v)
 
-    if allow_only(['i', 'n'], params):
+    if allow_only(['ignore', 'name'], params):
         # ignore files
-        print('I')
-    elif allow_only(['s'], params):
+        print('IGNORE')
+    elif allow_only(['store'], params):
         # store a session
-        print('S')
-    elif allow_only(['r'], params):
+        print('STORE')
+    elif allow_only(['restore'], params):
         # restore a session
-        print('R')
-    elif allow_only(['a'], params):
+        print('RESTORE')
+    elif allow_only(['list_all_apps'], params):
         # show all running apps
-        print('A')
-    elif allow_only(['la'], params):
+        print('LIST ALL APPS')
+    elif allow_only(['list_sessions'], params):
         # list all active sessions
-        print('LA')
+        print('LIST SESSIONS')
     # elif allow_only(['d'], params):
     #     decouples storage of apps from browser
     #     pass
