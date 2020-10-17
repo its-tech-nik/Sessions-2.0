6a73a675dea1dec893624701091d69f0e6a5f9db
Nikandros <m.nik1997@hotmail.com>
2020-10-17 19:05:41 +0100

    globalized helper functions

--- App.py
+++ App.py
@@ -1,7 +1,6 @@
 import sys, appdirs, os
 from appdirs import AppDirs
 
-
 class HelperFunctions():
     def touch(self, path):
         with open(path, 'a'):
@@ -12,27 +11,28 @@ class HelperFunctions():
             print('Session name not provided. You cannot use this method without passing a session name in the constructor')
             sys.exit()
 
+Helpers = HelperFunctions()
+
 class App():
 
     def __init__(self, session_name=None):
         self.session_name = session_name
         self.file_storage = AppDirs('Sessions').user_data_dir
-        self.helpers = HelperFunctions()
 
 
 
     def ignore(self, ignored_app):
-        self.helpers.check_for_session_name(self.session_name)
+        Helpers.check_for_session_name(self.session_name)
 
         print(f'We are about to ignore the app {ignored_app}')
 
     def store(self):
-        self.helpers.check_for_session_name(self.session_name)
+        Helpers.check_for_session_name(self.session_name)
 
         print(f'We are about to store the session {self.session_name} in {self.file_storage}')
 
     def restore(self):
-        self.helpers.check_for_session_name(self.session_name)
+        Helpers.check_for_session_name(self.session_name)
 
         print(f'We are about to restore the session {self.session_name}')
 
