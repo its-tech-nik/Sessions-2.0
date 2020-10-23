c7e18914707e41976e46becd8ffb26edd5c11e6c
Nikandros <m.nik1997@hotmail.com>
2020-10-24 00:36:49 +0100

    post-installation scrip (after all the libraries have been installed)

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup
+import subprocess
+from setuptools.command.install import install
+
+class InstallWrapper(install):
+    """
+    Overriding the default installation process in order
+    to setup the tab completion script in the right folder
+    with the appdirs module
+    """
+    def run(self):
+        pass
+        # from appdirs import AppDirs
+        # file_storage = AppDirs('Sessions').user_data_dir
+        # file_storage = file_storage.split()
+        # file_storage = "\ ".join(file_storage)
+        # subprocess.Popen(['bash', 'post-install.sh', f'\'{file_storage}\''])
+        # install.run(self)
+
 
 setup(
     name="Session 2.0",
@@ -8,4 +26,9 @@ setup(
         [console_scripts]
         session=src.main:cli
     """,
+    include_package_data=True,
+    cmdclass={
+        'install': InstallWrapper
+    },
 )
+
